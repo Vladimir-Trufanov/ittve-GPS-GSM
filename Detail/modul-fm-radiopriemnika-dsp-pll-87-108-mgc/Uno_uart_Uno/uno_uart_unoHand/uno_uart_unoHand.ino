c8f15a643515a5fd696ae77f2b2ef657acf9f71a
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2024-02-11 11:10:05 +0300

    LCD FM RX v2.0

---
+++ Detail/modul-fm-radiopriemnika-dsp-pll-87-108-mgc/Uno_uart_Uno/uno_uart_unoHand/uno_uart_unoHand.ino
@@ -0,0 +1,29 @@
+// *************************************************** uno_uart_unoHand.ino ***
+// * Подменить на отладке модуль радиоприемника LCD FM RX v2.0                *
+// * второй платой Arduino Uno и продублировать подаваемые команды            * 
+// *                                                                          *
+// * v1.0, 10.02.2024                              Автор:       Труфанов В.Е. *
+// * Copyright © 2024 tve                          Дата создания:  08.02.2024 *
+// ****************************************************************************
+
+#include <SoftwareSerial.h>
+SoftwareSerial serialRX(4,3);   // RX=4, TX=3
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  serialRX.begin(38400);  
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  serialRX.available()?digitalWrite(13,1):digitalWrite(13,0);
+  if (serialRX.available())
+  {
+    char simb=serialRX.read();
+    Serial.print(simb);
+  }
+}
+// *************************************************** uno_uart_unoHand.ino ***
+
