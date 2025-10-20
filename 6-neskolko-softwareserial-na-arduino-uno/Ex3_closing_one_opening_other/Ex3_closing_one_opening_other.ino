309cabc340ea2ab3da66fdd627edc8e13ace4cb7
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2025-10-20 11:29:28 +0300

    Первый пример совместной работы SIM900 и VKEL-TTL

--- 6-neskolko-softwareserial-na-arduino-uno/Ex3_closing_one_opening_other/Ex3_closing_one_opening_other.ino
+++ 6-neskolko-softwareserial-na-arduino-uno/Ex3_closing_one_opening_other/Ex3_closing_one_opening_other.ino
@@ -14,14 +14,14 @@
  * и в итоге через программный последовательный порт на пинах 7 = RX, 8 - TX, 
  * производится отправка AT-команд на SIM900.
  * 
- * v1.0.0, 16.10.2025                                 Автор:      Труфанов В.Е.
+ * v1.0.1, 20.10.2025                                 Автор:      Труфанов В.Е.
  * Copyright © 2025 tve                               Дата создания: 16.10.2025
  *
 **/
 
 #include <SoftwareSerial.h>
 SoftwareSerial VKEL_TTL(2,3); // синий на 2 - будет RX; зеленый на 3 - будет TX
-//SoftwareSerial   SIM900(7,8); // SIM900 
+SoftwareSerial   SIM900(7,8); // SIM900 
 unsigned long ncikl=0;
 
 // Настраиваем переменные и модули для работы с V.KEL TTL
@@ -35,29 +35,32 @@ unsigned long time, date;
 void setup()
 {
   Serial.begin(115200);
-
-  //VKEL_TTL.begin(9600); // скорость обмена с GPS-приемником
+  delay(500);
   Serial.println(" ");
-  Serial.println("Ожидаем ii разговора с V.KEL-TTL ...");
-
-  //SIM900.begin(9600);
-  //delay(500);
-  //update_SIM900();
-  //Talk_SIM900();
+  Serial.println("Очищаем буфер SIM900");
+  while (SIM900.available()) SIM900.read();
+  Serial.println("Ожидаем разговора с V.KEL-TTL ...");
 }
  
 void loop()
 {
   ncikl++;
-  // Делаем задержку в 1 секунду для того, чтобы буфер последовательного
-  // порта V.KEL-TTL заполнился данными с координатами
-  VKEL_TTL.begin(9600); 
-  delay(1000);
-  // Выбираем данные навигации из приёмника GPS V.KEL TTL 
-  Talk_VKEL_TTL();
-  VKEL_TTL.end();
+  if (ncikl>8) delay(1000);
+  else
+  {
+    // Делаем задержку в 1 секунду для того, чтобы буфер последовательного
+    // порта V.KEL-TTL заполнился данными с координатами
+    VKEL_TTL.begin(9600); 
+    delay(1000);
+    // Выбираем данные навигации из приёмника GPS V.KEL TTL 
+    Talk_VKEL_TTL();
+    VKEL_TTL.end();
 
-  //update_SIM900();
+    SIM900.begin(9600);
+    delay(500);
+    Talk_SIM900();
+    SIM900.end();
+  }
 }
 
 // ****************************************************************************
@@ -100,7 +103,7 @@ bool readgps()
   return false;
 }
 
-/*
+
 // ****************************************************************************
 // *               Выбрать ответное сообщение из буфера SIM900                *
 // ****************************************************************************
@@ -121,49 +124,59 @@ void update_SIM900()
 // ****************************************************************************
 void Talk_SIM900()
 {
-  Serial.println(" ");
-  Serial.println("Разговор с SIM900 ...");
-  delay(1000);
-  // AT
-  // Serial.println("Рукопожатие с SIM900. \"AT\" – Это самая простая команда AT.");
-  // Serial.println("Она также инициализирует передачу данных в бодах.");
-  // Serial.println("Если это сработает, далее вы будете видеть эхо-сигнал символов AT, а затем OK,");
-  // Serial.println("сообщающий вам, что все в порядке и SIM900 правильно вас понимает!");
-  SIM900.println("AT"); // Handshaking with SIM900
-  update_SIM900();
-  // AT+CSQ
-  // Serial.println("AT+CSQ – Проверить \"уровень сигнала\" – первое значение это уровень сигнала в дБ,");
-  // Serial.println("он должен быть выше 5. Чем выше, тем лучше, до 31.");
-  // Serial.println("Конечно, это зависит от вашей антенны и местоположения!");
-  SIM900.println("AT+CSQ"); // Signal quality test, value range is 0-31 , 31 is the best
-  update_SIM900();
-  // AT+CCID 
-  // Serial.println("AT+CCID – Получить номер SIM–карты,");
-  // Serial.println("это проверка того, что SIM-карта найдена нормально,");
-  // Serial.println("и вы можете проверить, какой номер присвоен карте.");
-  SIM900.println("AT+CCID"); // Read SIM information to confirm whether the SIM is plugged
-  update_SIM900();
-  // AT+CREG? 
-  // Serial.println("AT+CREG? Проверить, зарегистрированы ли вы в сети. Второе число должно быть");
-  // Serial.println("1 или 5. 1 означает, что вы зарегистрированы в домашней сети, а 5 - в роуминговой сети.");
-  // Serial.println("Кроме этих двух цифр, вы не зарегистрированы ни в какой сети.");
-  SIM900.println("AT+CREG?"); // Check whether it has registered in the network
-  update_SIM900();
-  // ATI
-  // Serial.println("ATI – Получить название платы и её редакцию");
-  SIM900.println("ATI"); 
-  update_SIM900();
-  // AT+COPS?
-  // Serial.println("AT+COPS? – Убедится, что вы подключены к сети и получить информацию об операторе");
-  SIM900.println("AT+COPS?"); 
-  update_SIM900();
-  // AT+CBC
-  Serial.println("AT+CBC – Получить состояние батареи lipo. Вторая цифра - это % заполнения,");
-  Serial.println("а третья цифра - фактическое напряжение в мВ.");
-  SIM900.println("AT+CBC"); 
-  update_SIM900();
+  int divisor=8;
+  Serial.print(ncikl); Serial.print(": "); 
+  if (ncikl % divisor == 1)
+  {
+    Serial.println(" ");
+    Serial.println("Разговор с SIM900 ...");
+    delay(1000);
+  }
+  else if (ncikl % divisor == 2)
+  {
+    // AT - Рукопожатие с SIM900. "AT" – Это самая простая команда AT. Она также инициализирует передачу данных в бодах
+    // Если это сработает, далее вы будете видеть эхо-сигнал символов AT, а затем OK, сообщающий вам, что все в порядке и SIM900 правильно вас понимает!
+    SIM900.println("AT"); // Handshaking with SIM900
+    update_SIM900();
+  }
+  else if (ncikl % divisor == 3)
+  {
+    // AT+CSQ – Проверить "уровень сигнала" – первое значение это уровень сигнала в дБ, он должен быть выше 5. Чем выше, тем лучше, до 31.
+    SIM900.println("AT+CSQ"); // Signal quality test, value range is 0-31 , 31 is the best
+    update_SIM900();
+  }
+  else if (ncikl % divisor == 4)
+  {
+    // AT+CCID – Получить номер SIM–карты, это проверка того, что SIM-карта найдена нормально, и вы можете проверить, какой номер присвоен карте
+    SIM900.println("AT+CCID"); // Read SIM information to confirm whether the SIM is plugged
+    update_SIM900();
+  }
+  else if (ncikl % divisor == 5)
+  {
+    // AT+CREG? Проверить, зарегистрированы ли вы в сети. Второе число должно быть 1 или 5. 1 означает, что вы зарегистрированы в домашней сети,
+    // а 5 - в роуминговой сети. Кроме этих двух цифр, вы не зарегистрированы ни в какой сети
+    SIM900.println("AT+CREG?"); // Check whether it has registered in the network
+    update_SIM900();
+  }
+  else if (ncikl % divisor == 6)
+  {
+    // ATI – Получить название платы и её редакцию
+    SIM900.println("ATI"); 
+    update_SIM900();
+  }
+  else if (ncikl % divisor == 7)
+  {
+    // AT+COPS? – Убедится, что вы подключены к сети и получить информацию об операторе
+    SIM900.println("AT+COPS?"); 
+    update_SIM900();
+  }
+  else if (ncikl % divisor == 0)
+  {
+    // AT+CBC – Получить состояние батареи lipo. Вторая цифра - это % заполнения, а третья цифра - фактическое напряжение в мВ
+    SIM900.println("AT+CBC"); 
+    update_SIM900();
+  }
 }
-*/
 
 // Arduino C/C++ ************************ Ex3_closing_one_opening_other.ino ***
                                                                                                                                                                                                   
