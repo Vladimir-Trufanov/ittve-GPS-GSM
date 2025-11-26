40ff61006489e4ce46a03d53ac8472616519e2d1
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2025-11-26 19:16:42 +0300

    Update AboutATcommand.ino

--- 1-SIM900-i-podklyuchenie-k-arduino/AboutATcommand/AboutATcommand.ino
+++ 1-SIM900-i-podklyuchenie-k-arduino/AboutATcommand/AboutATcommand.ino
@@ -7,10 +7,37 @@
  *
 **/
 
+#include <Regexp.h>
+
 #include <SoftwareSerial.h>
 
 // Configure software serial port
 SoftwareSerial SIM900(7,8); 
+
+
+// called for each match
+void match_callback  (const char * match,          // matching string (not null-terminated)
+                      const unsigned int length,   // length of matching string
+                      const MatchState & ms)      // MatchState in use (to get captures)
+{
+char cap [10];   // must be large enough to hold captures
+  
+  Serial.print ("Matched: ");
+  Serial.write ((byte *) match, length);
+  Serial.println ();
+  
+  for (byte i = 0; i < ms.level; i++)
+    {
+    Serial.print ("Capture "); 
+    Serial.print (i, DEC);
+    Serial.print (" = ");
+    ms.GetCapture (cap, i);
+    Serial.println (cap); 
+    }  // end of for each capture
+
+}  // end of match_callback 
+
+
 
 void setup() 
 {
@@ -35,6 +62,52 @@ void setup()
   // AT+CBC – Получить состояние батареи lipo. Вторая цифра - это % заполнения, а третья цифра - фактическое напряжение в мВ
   sendCommand("AT+CBC"," Получаем состояние батареи. Вторая цифра - это % заполнения, а третья цифра - фактическое напряжение в мВ"); 
   delay(1000);   
+
+  /*
+  // match state object
+  MatchState ms;
+
+  // what we are searching (the target)
+  char buf [100] = "The quick  brown fox jumps over the lazy wolf";
+  ms.Target (buf);  // set its address
+  Serial.println (buf);
+
+  //char result = ms.Match ("f.x");
+  char result = ms.Match ("%s%s(%a+)( )");
+  
+  if (result > 0)
+    {
+    Serial.print ("Found match at: ");
+    Serial.println (ms.MatchStart);        // 16 in this case     
+    Serial.print ("Match length: ");
+    Serial.println (ms.MatchLength);       // 3 in this case
+    }
+  else
+    Serial.println ("No match.");
+  */  
+
+  Serial.println ();
+  unsigned long count;
+
+  // what we are searching (the target)
+  char buf [100] = "The quick brown fox jumps over the lazy wolf";
+
+  // match state object
+  MatchState ms (buf);
+
+  // original buffer
+  Serial.println (buf);
+
+  // search for three letters followed by a space (two captures)
+  count = ms.GlobalMatch ("(%a+)( )", match_callback);
+
+  // show results
+  Serial.print ("Found ");
+  Serial.print (count);            // 8 in this case
+  Serial.println (" matches.");
+ 
+
+
 }
 
 void loop() 
