046b0a86498c28fbbc54cb4e1b0fc632fd86f41a
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2025-11-27 12:49:35 +0300

    Update UsingCustomFields.ino

--- 3-V-KEL-TTL/TinyGPSpp/UsingCustomFields/UsingCustomFields.ino
+++ 3-V-KEL-TTL/TinyGPSpp/UsingCustomFields/UsingCustomFields.ino
@@ -7,11 +7,8 @@
    извлечения широты, долготы, высоты и т.д. из предложений $GPGGA и 
    $GPRMC.  Но с помощью типа TinyGPSCustom вы можете извлекать
    другие поля NMEA даже из нестандартных предложений NMEA.
-
-   It requires the use of SoftwareSerial, and assumes that you have a
-   4800-baud serial GPS device hooked up on pins 4(RX) and 3(TX).
 */
-static const int RXPin = 4, TXPin = 3;
+static const int RXPin = 12, TXPin = 13;
 static const uint32_t GPSBaud = 9600;
 
 // The TinyGPSPlus object
@@ -21,17 +18,76 @@ TinyGPSPlus gps;
 SoftwareSerial ss(RXPin, TXPin);
 
 /*
-   By declaring TinyGPSCustom objects like this, we announce that we
-   are interested in the 15th, 16th, and 17th fields in the $GPGSA 
-   sentence, respectively the PDOP (F("positional dilution of precision")),
-   HDOP (F("horizontal...")), and VDOP (F("vertical...")).
+By declaring TinyGPSCustom objects like this, we announce that we
+are interested in the 15th, 16th, and 17th fields in the $GPGSA 
+sentence, respectively the PDOP (F("positional dilution of precision")),
+HDOP (F("horizontal...")), and VDOP (F("vertical...")).
 
-   (Counting starts with the field immediately following the sentence name, 
-   i.e. $GPGSA.  For more information on NMEA sentences, consult your
-   GPS module's documentation and/or http://aprs.gids.nl/nmea/.)
+Объявляя объекты TinyGPSCustom подобным образом, мы объявляем, что нас
+интересуют 15-е, 16-е и 17-е поля в предложении $GPGSA, 
+соответственно PDOP (F("позиционное снижение точности")),
+HDOP (F("горизонтальный...")) и VDOP (F("вертикальный...")).
 
-   If your GPS module doesn't support the $GPGSA sentence, then you 
-   won't get any output from this program.
+HDOP (Horizontal Dilution of Precision)
+---------------------------------------
+
+HDOP — параметр в системах глобального позиционирования (GPS), 
+который отражает точность горизонтальной составляющей положения (широта и долгота). 
+Он указывает на то, насколько точно может быть определено горизонтальное положение 
+в данных условиях. 
+
+HDOP зависит от геометрического расположения спутников относительно приёмника: 
+Низкое значение — спутники расположены далеко друг от друга (широкое покрытие неба).
+Высокое значение — спутники сгруппированы в одной области, что снижает точность.
+
+HDOP показывает, насколько велика погрешность координат из-за геометрического 
+расположения спутников, даже если сигналы от каждого из них получены без ошибок. 
+
+Некоторые значения HDOP и их интерпретация:
+0–1 — идеальная точность, характерна для открытых пространств и качественного оборудования при отличных условиях приёма и большом количестве спутников. 
+Координаты имеют погрешность в пределах 2–5 метров.
+1–3 — высокая точность, подходит для большинства задач, включая мониторинг транспорта и расчёт пробега. 
+Погрешность обычно в диапазоне 5–10 метров.
+3–6 — средняя точность, погрешность может достигать 20–50 метров, особенно если спутники находятся низко над горизонтом 
+или сгруппированы в одной части неба.
+6 и более — низкая точность, погрешность превышает 
+100 метров, данные становятся бесполезными. Такой уровень HDOP наблюдается в условиях 
+плотной городской застройки, в туннелях или при плохом расположении спутников.
+
+VDOP (Vertical Dilution of Precision)
+-------------------------------------
+
+VDOP — это снижение точности в вертикальной плоскости (высота) в системах глобального позиционирования. 
+VDOP измеряет влияние геометрии спутников на точность высоты, которую определяет устройство GPS. Он показывает, 
+как ошибки в положении спутников относительно друг друга могут влиять на точность данных о вертикальном расположении. 
+Чем ниже значение VDOP, тем выше точность данных о высоте.
+
+PDOP (Position Dilution of Precision)
+-------------------------------------
+
+PDOP — параметр в спутниковой навигации, который учитывает геометрическое расположение спутников 
+относительно приёмника на Земле. Он показывает, насколько точно может быть определено положение, включая горизонтальную (широта и долгота) 
+и вертикальную (высота) составляющие. 
+
+Значение PDOP:
+Низкое значение (например, 1–3) указывает на высокую точность определения положения.
+Высокое значение (например, 5–10) указывает на более низкую точность.
+ 
+Формула: PDOP^2 = HDOP^2 + VDOP^2. 
+
+GPS Altitude
+------------
+
+GPS Altitude — это измерение высоты объекта над уровнем моря. 
+Значение GPS Altitude определяется с помощью технологии Глобальной системы позиционирования (GPS). Для расчёта используется эллипсоид всей Земли. 
+Важно учитывать, что значение GPS Altitude не всегда точно указывает на высоту объекта над землёй. На него могут влиять различные факторы, 
+такие как атмосферное давление, температура и точность сигнала. 
+
+(Counting starts with the field immediately following the sentence name, 
+i.e. $GPGSA.  For more information on NMEA sentences, consult your
+GPS module's documentation and/or http://aprs.gids.nl/nmea/.)
+If your GPS module doesn't support the $GPGSA sentence, then you 
+won't get any output from this program.
 */
 
 TinyGPSCustom pdop(gps, "GPGSA", 15); // $GPGSA sentence, 15th element
